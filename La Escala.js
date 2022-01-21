407d605ef8179a5c6d2bb27bc323ecaa90cd0fca
MarcosHerasme <97996630+MarcosHerasme@users.noreply.github.com>
2022-01-20 21:05:13 -0400

    Add files via upload

---
+++ La Escala.js
@@ -0,0 +1,214 @@
+// Declaración de las Constantes para la captura del formulario.
+
+    const form = document.getElementById("form");
+    const k13 = document.getElementsByName('k13');
+    const k14 = document.getElementsByName('k14');
+    const k15 = document.getElementsByName('k15');
+    const k16 = document.getElementsByName('k16');
+    const k17 = document.getElementsByName('k17');
+    const k18 = document.getElementsByName('k18');
+    const k19 = document.getElementsByName('k19');
+    const k20 = document.getElementsByName('k20');
+    const k21 = document.getElementsByName('k21');
+    const k22 = document.getElementsByName('k22');
+    const k23 = document.getElementsByName('k23');
+    const k24 = document.getElementsByName('k24');
+    const k25 = document.getElementsByName('k25');
+    const k26 = document.getElementsByName('k26');
+    const k27 = document.getElementsByName('k27');
+    const k28 = document.getElementsByName('k28');
+    const k29 = document.getElementsByName('k29');
+    const k30 = document.getElementsByName('k30');
+    const k31 = document.getElementsByName('k31');
+    const k32 = document.getElementsByName('k32');
+    const k33 = document.getElementsByName('k33');
+    const k34 = document.getElementsByName('k34');
+    const k35 = document.getElementsByName('k35');
+    const k36 = document.getElementsByName('k36');
+    const k37 = document.getElementsByName('k37');
+    const k38 = document.getElementsByName('k38');
+    const k39 = document.getElementsByName('k39');
+    const k40 = document.getElementsByName('k40');
+    const k41 = document.getElementsByName('k41');
+    const k42 = document.getElementsByName('k42');
+    const k43 = document.getElementsByName('k43');
+    const k44 = document.getElementsByName('k44');
+    const k45 = document.getElementsByName('k45');
+    const k46 = document.getElementsByName('k46');
+    const k47 = document.getElementsByName('k47');
+    const k48 = document.getElementsByName('k48');
+    const k49 = document.getElementsByName('k49');
+    const k50 = document.getElementsByName('k50');
+    const k51 = document.getElementsByName('k51');
+    const k52 = document.getElementsByName('k52');
+    const k53 = document.getElementsByName('k53');
+    const k54 = document.getElementsByName('k54');
+    const k55 = document.getElementsByName('k55');
+    const k56 = document.getElementsByName('k56');
+    const k57 = document.getElementsByName('k57');
+    const k58 = document.getElementsByName('k58');
+
+
+// Función de captura de los valores.
+
+function escala() {
+	
+	// Variables Locales valor de las respuestas.
+    var k13A = 0;
+    var k14A = 0;
+    var k15A = 0;
+    var k16A = 0;
+    var k17A = 0;
+    var k18A = 0;
+    var k19A = 0;
+    var k20A = 0;
+    var k21A = 0;
+    var k22A = 0;
+    var k23A = 0;
+    var k24A = 0;
+    var k25A = 0;
+    var k26A = 0;
+    var k27A = 0;
+    var k28A = 0;
+    var k29A = 0;
+    var k30A = 0;
+    var k31A = 0;
+    var k32A = 0;
+    var k33A = 0;
+    var k34A = 0;
+    var k35A = 0;
+    var k36A = 0;
+    var k37A = 0;
+    var k38A = 0;
+    var k39A = 0;
+    var k40A = 0;
+    var k41A = 0;
+    var k42A = 0;
+    var k43A = 0;
+    var k44A = 0;
+    var k45A = 0;
+    var k46A = 0;
+    var k47A = 0;
+    var k48A = 0;
+    var k49A = 0;
+    var k50A = 0;
+    var k51A = 0;
+    var k52A = 0;
+    var k53A = 0;
+    var k54A = 0;
+    var k55A = 0;
+    var k56A = 0;
+    var k57A = 0;
+    var k58A = 0;
+	
+   for(var x = 0; x < 6; x++){
+        if(k13[x].checked) { k13A = Number(k13[x].value); }
+ //     if(k14[x].checked) { k14A = Number(k14[x].value); }
+        if(k15[x].checked) { k15A = Number(k15[x].value); }
+        if(k16[x].checked) { k16A = Number(k16[x].value); }
+        if(k17[x].checked) { k17A = Number(k17[x].value); }
+        if(k18[x].checked) { k18A = Number(k18[x].value); }
+        if(k19[x].checked) { k19A = Number(k19[x].value); }
+        if(k20[x].checked) { k20A = Number(k20[x].value); }
+        if(k21[x].checked) { k21A = Number(k21[x].value); }
+        if(k22[x].checked) { k22A = Number(k22[x].value); }
+        if(k23[x].checked) { k23A = Number(k23[x].value); }
+        if(k24[x].checked) { k24A = Number(k24[x].value); }
+        if(k25[x].checked) { k25A = Number(k25[x].value); }
+        if(k26[x].checked) { k26A = Number(k26[x].value); }
+//      if(k27[x].checked) { k27A = Number(k27[x].value); }
+        if(k28[x].checked) { k28A = Number(k28[x].value); }
+        if(k29[x].checked) { k29A = Number(k29[x].value); }
+        if(k30[x].checked) { k30A = Number(k30[x].value); }
+        if(k31[x].checked) { k31A = Number(k31[x].value); }
+        if(k32[x].checked) { k32A = Number(k32[x].value); }
+//      if(k33[x].checked) { k33A = Number(k33[x].value); }
+        if(k34[x].checked) { k34A = Number(k34[x].value); }
+        if(k35[x].checked) { k35A = Number(k35[x].value); }
+        if(k36[x].checked) { k36A = Number(k36[x].value); }
+        if(k37[x].checked) { k37A = Number(k37[x].value); }
+//      if(k38[x].checked) { k38A = Number(k38[x].value); }
+        if(k39[x].checked) { k39A = Number(k39[x].value); }
+        if(k40[x].checked) { k40A = Number(k40[x].value); }
+        if(k41[x].checked) { k41A = Number(k41[x].value); }
+        if(k42[x].checked) { k42A = Number(k42[x].value); }
+        if(k43[x].checked) { k43A = Number(k43[x].value); }
+        if(k44[x].checked) { k44A = Number(k44[x].value); }
+        if(k45[x].checked) { k45A = Number(k45[x].value); }
+        if(k46[x].checked) { k46A = Number(k46[x].value); }
+        if(k47[x].checked) { k47A = Number(k47[x].value); }
+//      if(k48[x].checked) { k48A = Number(k48[x].value); }
+        if(k49[x].checked) { k49A = Number(k49[x].value); }
+        if(k50[x].checked) { k50A = Number(k50[x].value); }
+        if(k51[x].checked) { k51A = Number(k51[x].value); }
+        if(k52[x].checked) { k52A = Number(k52[x].value); }
+        if(k53[x].checked) { k53A = Number(k53[x].value); }
+//      if(k54[x].checked) { k54A = Number(k54[x].value); }
+        if(k55[x].checked) { k55A = Number(k55[x].value); }
+        if(k56[x].checked) { k56A = Number(k56[x].value); }
+        if(k57[x].checked) { k57A = Number(k57[x].value); }
+        if(k58[x].checked) { k58A = Number(k58[x].value); }
+       
+// Estos son los campos con valores binarios
+       
+    if(k14[0].checked) { k14A = Number(k14[0].value); }
+        else if(k14[1].checked) { k14A = Number(k14[1].value); }
+        else k14A=0;
+    if(k27[0].checked) { k27A = Number(k27[0].value); }
+        else if(k27[1].checked) { k27A = Number(k27[1].value); }
+        else k27A=0;
+    if(k33[0].checked) { k33A = Number(k33[0].value); }
+        else if(k33[1].checked) { k33A = Number(k33[1].value); }
+        else k33A=0;
+    if(k38[0].checked) { k38A = Number(k38[0].value); }
+        else if(k38[1].checked) { k38A = Number(k38[1].value); }
+        else k38A=0;
+    if(k48[0].checked) { k48A = Number(k48[0].value); }
+        else if(k48[1].checked) { k48A = Number(k48[1].value); }
+        else k48A=0;		
+    if(k54[0].checked) { k54A = Number(k54[0].value); }
+        else if(k54[1].checked) { k54A = Number(k54[1].value); }
+        else k54A=0;
+ 
+   }
+  
+// Calculo de los resultados
+
+var d4 = k15A+k17A+k31A+k44A+k52A+k54A+k55A+k56A;
+var d5 = k14A+k16A+k23A+k24A+k25A+k26A+k33A+k39A+k45A+k46A+k47A+k48A+k53A+k57A;
+var d6 = k21A+k22A+k27A+k29A+k30A+k35A+k36A+k38A+k40A+k43A+k58A;
+var d7 = k13A+k18A+k19A+k20A+k28A+k32A+k34A+k37A+k41A+k42A+k49A+k50A+k51A;
+var d8 = d4 + d5 + d6 + d7;
+var e4 = (d4 + d5) / 2;
+var e6 = (d6 + d7) / 2;
+var g2 = e4 + e6;
+
+
+document.getElementById('d4').innerHTML = d4;
+document.getElementById('d5').innerHTML = d5; 
+document.getElementById('d6').innerHTML = d6;
+document.getElementById('d7').innerHTML = d7;
+document.getElementById('d8').innerHTML = d8;
+document.getElementById('e4').innerHTML = e4;
+document.getElementById('e6').innerHTML = e6;
+document.getElementById('g2').innerHTML = g2;
+
+} // Fin de la función escala()
+
+// Reinicia el formulario
+
+const reset = () => {
+	const radioButtons = document.querySelectorAll("input[type='radio']");
+	radioButtons.forEach(radioButtons => {
+			radioButtons.checked = false;})
+	
+document.getElementById('d4').innerHTML = "";
+document.getElementById('d5').innerHTML = ""; 
+document.getElementById('d6').innerHTML = "";
+document.getElementById('d7').innerHTML = "";
+document.getElementById('d8').innerHTML = "";
+document.getElementById('e4').innerHTML = "";
+document.getElementById('e6').innerHTML = "";
+document.getElementById('g2').innerHTML = "";
+	
+}
